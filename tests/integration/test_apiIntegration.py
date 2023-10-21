261704465bb9944dcb5ba207787e7a94187e0337
Fedi Hamdi <fedihamdi.jr@gmail.com>
2023-10-21 08:16:26 +0200

    Update test_apiIntegration.py

---
+++ tests/integration/test_apiIntegration.py
@@ -0,0 +1,21 @@
+import os
+
+import cdsapi
+
+
+def test_request():
+    c = cdsapi.Client()
+
+    r = c.retrieve(
+        "reanalysis-era5-single-levels",
+        {
+            "variable": "2t",
+            "product_type": "reanalysis",
+            "date": "2012-12-01",
+            "time": "12:00",
+        },
+    )
+
+    r.download("test.grib")
+
+    assert os.path.getsize("test.grib") == 2076600
