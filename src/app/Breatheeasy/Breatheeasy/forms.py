b7a8b5f5b696aba4fb38d72e6abf1351cd95c899
Fedi Hamdi <fedihamdi.jr@gmail.com>
2023-11-26 15:33:45 +0100

    [Deprecate] Unused templates

--- src/app/Breatheeasy/Breatheeasy/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-
-class PollenUserLog(forms.Form):
-    title = forms.CharField()
-    description = forms.CharField()
-    views = forms.IntegerField()
-    available = forms.BooleanField()
